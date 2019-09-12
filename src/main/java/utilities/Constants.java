7a2bfd563f24000934c7b8a15124fd484b48e67c
Shubham Jaiswal <shubham.jaiswal@synapse.com>
2019-09-12 12:36:30 +0530

    new file

--- src/main/java/utilities/Constants.java
+++ src/main/java/utilities/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 	
 	public final static String URL="https://www.google.com";
 	public final static String BROWSER="Chrome";
-	public final static String EXCELPATH="";
+	public final static String EXCELPATH="Documents";
 	public final static String DRIVERPATH="chromedriver_v75.exe";
 }
